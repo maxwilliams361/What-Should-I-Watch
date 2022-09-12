407114c570589effb6a5e0a9c105c982d680c1cf
maxwilliams361 <52715538+maxwilliams361@users.noreply.github.com>
2022-09-12 17:10:55 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+let horrorButton = document.querySelector(".horror-button"); 
+let horrorRecommend = document.querySelector(".horror-recommend");
+
+
+let horror = ["Midsommar", "Hereditary", "Get Out"];
+horrorButton.onclick=function(){
+    /* Write a for of loop around the code below!
+     horrorRecommend.insertAdjacentHTML=("beforeend",_____);
+*/
+
+};
+
+
